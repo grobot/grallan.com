1f21a55b67e11c8a775d02bc469c6866bb32396c
Greg Allan <gregory.j.allan@gmail.com>
2014-12-12 12:51:14 -0800

    update to styles + robot

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,21 @@
 exports.init = function(app) {
   app.get('/',
     index
-  )
-}
+  );
+  app.get('/projects',
+    projects
+  );
+  app.get('/about',
+    about
+  );
+};
 
 function index(req, res, next) {
-  res.render('index')
+  res.render('index');
+}
+function projects(req, res, next) {
+  res.render('projects');
+}
+function about(req, res, next) {
+  res.render('about');
 }
